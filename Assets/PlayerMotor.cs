0a8d53cb88efbf3dd8960a800b63fd5cacd4bea9
benjeee <bentomonline@gmail.com>
2018-01-08 21:36:10 -0500

    init assets

---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+	[SerializeField]
+	private Camera cam;
+
+	private Vector3 velocity = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private Vector3 camRotation = Vector3.zero;
+
+	private Rigidbody rb;
+
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+		
+	public void Move(Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+
+	public void Rotate(Vector3 _rotation)
+	{
+		rotation = _rotation;
+	}
+
+	public void CamRotate(Vector3 _camRotation)
+	{
+		camRotation = _camRotation;
+	}
+
+	//run every physics tick
+	void FixedUpdate()
+	{
+		PerformMovement ();
+		PerformRotation ();
+	}
+
+	private void PerformMovement()
+	{
+		if (velocity != Vector3.zero) 
+		{
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+
+	private void PerformRotation()
+	{
+		rb.MoveRotation (rb.rotation * Quaternion.Euler (rotation));
+		if (cam != null) 
+		{
+			cam.transform.Rotate (camRotation);
+		}
+	}
+}
---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Behaviour[] componentsToDisable;
+
+	Camera sceneCamera;
+
+	void Start() {
+		if (!isLocalPlayer) {
+			Cursor.visible = true;
+			for (int i = 0; i < componentsToDisable.Length; i++) {
+				componentsToDisable [i].enabled = false;
+			}
+		} else {
+			Cursor.visible = false;
+			sceneCamera = Camera.main;
+			if (sceneCamera != null) {
+				sceneCamera.gameObject.SetActive(false);
+			}
+		}
+	}
+
+	void OnDisable(){
+		if (sceneCamera != null) {
+			sceneCamera.gameObject.SetActive(true);
+		}
+	}
+}
